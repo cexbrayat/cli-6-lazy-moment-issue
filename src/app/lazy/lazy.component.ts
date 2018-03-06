7d8d24692ee86812c2cb44de3775051d501be42d
cexbrayat <cedric@ninja-squad.com>
2018-03-06 12:06:11 +0100

    add moment

--- src/app/lazy/lazy.component.ts
+++ src/app/lazy/lazy.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-lazy',
   template: `lazy`
 })
 export class LazyComponent {
-  title = 'lazy';
+  title = moment();
 }
